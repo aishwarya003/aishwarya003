4758efb830d51481562c77f81fc42dcf2258f4f8
aishwarya003 <aishwarya.t2311@gmail.com>
2016-10-21 12:38:27 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("Enter the number");
+scanf("%d",&num);
+if(num>0)
+{
+ printf("positive");
+}
+else if(num<0)
+{
+  printf("negative");
+}
+else
+ printf("0");
+}
