217b8dd9cfe56fec6fc58964c6a5865207d567c6
Aishwarya <aishwarya.t2311@gmail.com>
2017-07-19 16:12:23 +0530

    Add files via upload

---
+++ set13_130.java
@@ -0,0 +1,25 @@
+package hunter;
+
+public class set13_130 {
+	public static void main(String[] args){
+		
+		days("mon");
+		days("sun");
+		}
+	public static void days(String s){
+		//String ss[]=new String[]{"mon","tue","wed","thu","fri","sat"};
+		try{
+			if(s!="sun"){
+		
+			System.out.println("true");
+			}else{
+				System.out.println("false");
+			}
+			
+		}
+		
+		catch(Exception e){
+			
+		}
+		}
+}
