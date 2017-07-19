43b46f7a87904f4786edb4d021cf519951de9d4f
Aishwarya <aishwarya.t2311@gmail.com>
2017-07-19 23:13:27 +0530

    Add files via upload

---
+++ hunterset10_92.java
@@ -0,0 +1,13 @@
+package Guvi;
+
+
+
+public class hunterset10_92 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		space("this  is  my  laptop");
+}
+	public static void space(String s){
+		System.out.println(s.replaceAll("\\s+"," "));
+	}
+}
