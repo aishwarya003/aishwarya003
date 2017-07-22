fef5ecedc903fb6a17ffb91df3ebe4eff88d8921
Aishwarya <aishwarya.t2311@gmail.com>
2017-07-22 13:57:33 +0530

    Add files via upload

---
+++ Set6_51.java
@@ -0,0 +1,15 @@
+package hunter;
+
+public class Set6_51 {
+	public static String output;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		string("32","3");
+		System.out.println(output);
+	}
+	public static void string(String a,String b){
+		int  c=Integer.valueOf(a)*Integer.valueOf(b);
+		output=String.valueOf(c);
+	}
+
+}
