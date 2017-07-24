a4c1296ac42ea4bdf677c2d07442490fede43757
Aishwarya <aishwarya.t2311@gmail.com>
2017-07-24 11:32:09 +0530

    Add files via upload

---
+++ Set5_48.java
@@ -0,0 +1,18 @@
+package hunter;
+
+import java.util.Scanner;
+
+public class Set5_48 {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	String ss=s.next();
+	String sub=s.next();
+	if(ss.contains(sub)){
+		System.out.println(ss.indexOf(sub));
+	}
+	else{
+		System.out.println();
+	
+}
+}
+}
