4e41f338860f99d910e6673cedd9819d054c34f2
Aishwarya <aishwarya.t2311@gmail.com>
2017-07-26 20:07:34 +0530

    Add files via upload

---
+++ HunterSet2_12.java
@@ -0,0 +1,29 @@
+package Guvi;
+
+import java.util.Scanner;
+
+public class HunterSet2_12 {
+public static void main(String[] args) {
+	Scanner s =new Scanner(System.in);
+	int a[]=new int[5];
+	int temp;
+	for(int i=0;i<5;i++){
+		a[i]=s.nextInt();
+	}
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+			if(a[i]>a[j]){
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	
+	System.out.println("Enter the kth value");
+	int k=s.nextInt();
+	System.out.println("The height of 4th tallest"+a[3]);
+	System.out.println("The height of kth tallest"+a[k-1]);
+	s.close();
+}
+}
