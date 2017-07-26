3d9c76191116081199b260f87a453e52b7c01a12
Aishwarya <aishwarya.t2311@gmail.com>
2017-07-26 20:10:21 +0530

    Add files via upload

--- HunterSet2_12.java
+++ HunterSet2_12.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class HunterSet2_12 {
 public static void main(String[] args) {
 	Scanner s =new Scanner(System.in);
-	int a[]=new int[5];
+	int a[]=new int[50];
 	int temp;
-	for(int i=0;i<5;i++){
+	for(int i=0;i<50;i++){
 		a[i]=s.nextInt();
 	}
-	for(int i=0;i<5;i++){
-		for(int j=0;j<5;j++){
+	for(int i=0;i<50;i++){
+		for(int j=0;j<50;j++){
 			if(a[i]>a[j]){
 				temp=a[i];
 				a[i]=a[j];
