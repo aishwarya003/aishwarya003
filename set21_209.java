abbff34749d90040c71b7b5995319835d7a8a7be
Aishwarya <aishwarya.t2311@gmail.com>
2017-08-17 20:56:17 +0530

    Add files via upload

---
+++ set21_209.java
@@ -0,0 +1,13 @@
+package Guvi;
+import java.util.*;
+public class set21_209 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner (System.in);
+String sd=s.next();
+String vg=s.next();
+System.out.println(sd+vg);
+	}
+
+}
