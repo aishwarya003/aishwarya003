27f7a1947a16aa6fdba7748580716c25b371550a
Aishwarya <aishwarya.t2311@gmail.com>
2017-08-21 09:40:38 +0530

    Add files via upload

---
+++ SumOfOddNumbersFrom0to20.java
@@ -0,0 +1,12 @@
+package player;
+
+public class SumOfOddNumbersFrom0to20  {
+public static void main(String[] args) {
+int sum=0;	
+for(int i=0;i<=20;i++){
+	if(i%2==1){
+		sum+=i;
+		}
+	}System.out.println(sum);
+}
+}
